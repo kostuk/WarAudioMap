2d065a266e71bb7f2bf6f5c420928e8a9fe969c4
NIkolas <nikolas@ukr.net>
2022-03-14 09:43:49 +0200

    Add new wav

---
+++ traning.py
@@ -0,0 +1,142 @@
+import os
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_io as tfio
+
+
+DATASET_PATH = 'data'
+
+data_dir = pathlib.Path(DATASET_PATH)
+
+commands = np.array(tf.io.gfile.listdir(str(data_dir)))
+commands = commands[ commands != 'none']
+print('Commands:', commands)
+
+filenames = tf.io.gfile.glob(str(data_dir) + '/*/*')
+filenames = tf.random.shuffle(filenames)
+num_samples = len(filenames)
+print('Number of total examples:', num_samples)
+print('Number of examples per label:',
+      len(tf.io.gfile.listdir(str(data_dir/commands[0]))))
+print('Example file tensor:', filenames[0])
+
+train_files = filenames[:100]
+val_files = filenames[100: 100 + 40]
+test_files = filenames[-40:]
+
+print('Training set size', len(train_files))
+print('Validation set size', len(val_files))
+print('Test set size', len(test_files))
+
+test_file = tf.io.read_file(DATASET_PATH+'/unknow/017.wav')
+test_audio, _ = tf.audio.decode_wav(contents=test_file)
+print(test_audio.shape)
+
+def decode_audio(audio_binary):
+  # Decode WAV-encoded audio files to `float32` tensors, normalized
+  # to the [-1.0, 1.0] range. Return `float32` audio and a sample rate.
+  audio, _ = tf.audio.decode_wav(contents=audio_binary)
+  # Since all the data is single channel (mono), drop the `channels`
+  # axis from the array.
+  return tf.squeeze(audio, axis=-1)
+
+def get_label(file_path):
+  parts = tf.strings.split(
+      input=file_path,
+      sep=os.path.sep)
+  # Note: You'll use indexing here instead of tuple unpacking to enable this
+  # to work in a TensorFlow graph.
+  return parts[-2]
+
+def get_waveform_and_label(file_path):
+  label = get_label(file_path)
+  audio_binary = tf.io.read_file(file_path)
+  waveform = decode_audio(audio_binary)
+  return waveform, label
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+files_ds = tf.data.Dataset.from_tensor_slices(train_files)
+
+waveform_ds = files_ds.map(
+    map_func=get_waveform_and_label,
+    num_parallel_calls=AUTOTUNE)
+
+
+rows = 3
+cols = 3
+n = rows * cols
+fig, axes = plt.subplots(rows, cols, figsize=(10, 12))
+
+for i, (audio, label) in enumerate(waveform_ds.take(n)):
+  r = i // cols
+  c = i % cols
+  ax = axes[r][c]
+  ax.plot(audio.numpy())
+  ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
+  label = label.numpy().decode('utf-8')
+  ax.set_title(label)
+
+plt.show()
+
+def get_spectrogram(waveform):
+  # Zero-padding for an audio waveform with less than 16,000 samples.
+  input_len = 16000
+  waveform = waveform[:input_len]
+  zero_padding = tf.zeros(
+      [16000] - tf.shape(waveform),
+      dtype=tf.float32)
+  # Cast the waveform tensors' dtype to float32.
+  waveform = tf.cast(waveform, dtype=tf.float32)
+  # Concatenate the waveform with `zero_padding`, which ensures all audio
+  # clips are of the same length.
+  equal_length = tf.concat([waveform, zero_padding], 0)
+  # Convert the waveform to a spectrogram via a STFT.
+  spectrogram = tf.signal.stft(
+      equal_length, frame_length=255, frame_step=128)
+  # Obtain the magnitude of the STFT.
+  spectrogram = tf.abs(spectrogram)
+  # Add a `channels` dimension, so that the spectrogram can be used
+  # as image-like input data with convolution layers (which expect
+  # shape (`batch_size`, `height`, `width`, `channels`).
+  spectrogram = spectrogram[..., tf.newaxis]
+  return spectrogram
+
+
+for waveform, label in waveform_ds.take(1):
+  label = label.numpy().decode('utf-8')
+  spectrogram = get_spectrogram(waveform)
+
+print('Label:', label)
+print('Waveform shape:', waveform.shape)
+print('Spectrogram shape:', spectrogram.shape)
+print('Audio playback')
+
+
+def plot_spectrogram(spectrogram, ax):
+  if len(spectrogram.shape) > 2:
+    assert len(spectrogram.shape) == 3
+    spectrogram = np.squeeze(spectrogram, axis=-1)
+  # Convert the frequencies to log scale and transpose, so that the time is
+  # represented on the x-axis (columns).
+  # Add an epsilon to avoid taking a log of zero.
+  log_spec = np.log(spectrogram.T + np.finfo(float).eps)
+  height = log_spec.shape[0]
+  width = log_spec.shape[1]
+  X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
+  Y = range(height)
+  ax.pcolormesh(X, Y, log_spec)
+  
+  
+fig, axes = plt.subplots(2, figsize=(12, 8))
+timescale = np.arange(waveform.shape[0])
+axes[0].plot(timescale, waveform.numpy())
+axes[0].set_title('Waveform')
+axes[0].set_xlim([0, 16000])
+
+plot_spectrogram(spectrogram.numpy(), axes[1])
+axes[1].set_title('Spectrogram')
+plt.show()
